e2a89131da39aab9ac39d4be57c5d044cae69a28
mullera3 <amani.k.muller@gmail.comClotilda1_>
2022-03-17 09:27:53 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public  String greeting;
+
+    Main()
+    {
+
+    }
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.greeting = "Hello World!";
+
+        System.out.println(main.greeting);
+    }
+}
