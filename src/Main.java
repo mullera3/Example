ca2b18c3a3d35bda20d2c60e2945e79d7f23b919
mullera3 <amani.k.muller@gmail.comClotilda1_>
2022-03-22 09:41:32 -0400

    New lines of code!

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,14 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         main.greeting = "Hello World!";
-
         System.out.println(main.greeting);
+
+        System.out.println("Github is awesome.");
+        System.out.println("Github is awesome.");
+        System.out.println("Github is awesome.");
+        System.out.println("Github is awesome.");
+        System.out.println("Github is awesome.");
+
+
     }
 }
